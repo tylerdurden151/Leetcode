e3f27eb68bbb1091f6fe6b3d75beffeeb68a7145
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-21 15:59:00 -0700

    Added Comments

---
+++ Concatenation of Array/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+1929. Concatenation of Array
+
+Given an integer array nums of length n, 
+you want to create an array ans of length 2n where ans[i] == nums[i] 
+and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+ */
+int[] value = { 1, 2, 1 };
+int[] value2 = { 1, 3, 2, 1 };
+Solution s = new Solution();
+Console.WriteLine(string.Join(", ", s.GetConcatenation(value))); 
+Console.WriteLine(string.Join(", ", s.GetConcatenation(value2))); ;
+
+
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] result = new int[nums.Length * 2];
+        int i = 0;
+        foreach (int n in nums)
+        {
+            result[i] = n;
+            result[i + nums.Length] = n;
+            i++;
+        }
+        return result;
+    }
+}
