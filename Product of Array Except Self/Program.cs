ca30f70fd1e89bfff6c0bf094289fd58f6b9b1cf
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-11 10:11:07 -0700

    Completed

---
+++ Product of Array Except Self/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Given an integer array nums, return an array
+ * answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ */
+Solution s = new Solution();
+int[] result = s.ProductofArray(new int[] { 1, 2, 3, 4 });
+Console.WriteLine(string.Join(", ", result));
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        // Calculate the product of all elements to the left of each element
+        result[0] = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Calculate the product of all elements to the right of each element
+        int rightProduct = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
