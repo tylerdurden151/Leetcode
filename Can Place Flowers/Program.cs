38b9956bad2978e75bf468815d780dc806a5861d
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-12 11:00:14 -0700

    Added readable solution

--- Can Place Flowers/Program.cs
+++ Can Place Flowers/Program.cs
@@ -48,3 +48,27 @@ public class Solution
         return count >= n;
     }
 }
+
+//Cleaner readable solution
+
+public class Solution2
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+
+           
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+            {
+
+                flowerbed[i] = 1;
+
+                n--;
+
+            }
+        }
+
+        return (n <= 0);
+    }
+}
