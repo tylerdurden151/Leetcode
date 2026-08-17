668cdc702a1f31cab44534a0b60df60811dcfaa5
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-17 10:35:29 -0700

    Added comments

--- Sort an Array/Program.cs
+++ Sort an Array/Program.cs
@@ -1,4 +1,20 @@
-﻿Solution s = new Solution();
+﻿/*
+ Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), 
+while the positions of other numbers are changed (for example, 1 and 5).
+ 
+ */
+
+Solution s = new Solution();
 int[] result1 = s.SortArray(new int[] { 88, 90, 77, 100 });
 int[] result2 = s.SortArray2(new int[] { 88, 90, 77, 100 });
 Console.WriteLine(string.Join(", ", result1));
