e56e1e9cbe5319790aa2e5b769bda8a7a120be52
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-20 09:22:34 -0700

    Completed

---
+++ Reverse Integer/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21*/
+
+Solution s = new Solution();
+Console.WriteLine(s.Reverse(123)); // Output: 321
+Console.WriteLine(s.Reverse(-123)); // Output: -321
+Console.WriteLine(s.Reverse(120)); // Output: 21
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        long rev = 0;
+        while (x != 0)
+        {
+            int pop = x % 10;
+            x /= 10;
+            rev = rev * 10 + pop;
+            if (rev > int.MaxValue || rev < int.MinValue)
+                return 0;
+        }
+        return (int)rev;
+    }
+}
