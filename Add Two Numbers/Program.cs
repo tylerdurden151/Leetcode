1dff6230948eee350d410dec250b71fbf38e822e
Tim Eckart <tylerdurden_151@yahoo.com>
2026-09-11 08:46:10 -0700

    Added Listnode

--- Add Two Numbers/Program.cs
+++ Add Two Numbers/Program.cs
@@ -20,6 +20,19 @@ Output: [8,9,9,9,0,0,0,1]
  */
 
 
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+     }
+ }
+
+
 public class Solution
 {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
